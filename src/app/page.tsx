77de84572b42d5757b0630393b61b8fe629eb9b1
Aman-Meenia <amanmeenia09876@gmail.com>
2024-10-03 16:25:09 +0530

    testing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ export default function Home() {
     <div>
       <h1>Hello Aman How are you </h1>
       <h1> CI-CD Workign in real time </h1>
-      <h1> Checkin....</h1>
+      <h1> Checking....</h1>
     </div>
   );
 }
